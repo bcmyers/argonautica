272a1f8a57302003851866011266a60c3d830719
Brian Myers <brian.carl.myers@gmail.com>
2018-04-27 20:31:16 -0500

    update

--- build.rs
+++ build.rs
@@ -2,47 +2,34 @@ extern crate bindgen;
 extern crate cc;
 
 use std::env;
-use std::fs;
 use std::path::Path;
 
-#[cfg_attr(rustfmt, rustfmt_skip)]
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let out_dir = Path::new(&out_dir);
-    if out_dir.exists() {
-        fs::remove_dir_all(out_dir).unwrap();
-    }
-    fs::create_dir_all(out_dir).unwrap();
-
-    let base_dir = Path::new("/Users/bcmyers/dev/a2");
-    cc::Build::new()
+    let mut builder = cc::Build::new();
+    builder
         .files(&[
-            base_dir.join("external/phc-winner-argon2/src/argon2.c"),
-            base_dir.join("external/phc-winner-argon2/src/bench.c"),
-            base_dir.join("external/phc-winner-argon2/src/core.c"),
-            base_dir.join("external/phc-winner-argon2/src/blake2/blake2b.c"),
-            base_dir.join("external/phc-winner-argon2/src/encoding.c"),
-            base_dir.join("external/phc-winner-argon2/src/genkat.c"),
-            // base_dir.join("external/phc-winner-argon2/src/opt.c"),
-            base_dir.join("external/phc-winner-argon2/src/ref.c"),
-            base_dir.join("external/phc-winner-argon2/src/run.c"),
-            base_dir.join("external/phc-winner-argon2/src/test.c"),
-            base_dir.join("external/phc-winner-argon2/src/thread.c"),
+            "phc-winner-argon2/src/argon2.c",
+            "phc-winner-argon2/src/core.c",
+            "phc-winner-argon2/src/blake2/blake2b.c",
+            "phc-winner-argon2/src/encoding.c",
+            "phc-winner-argon2/src/ref.c",
+            "phc-winner-argon2/src/thread.c",
+            // "phc-winner-argon2/src/opt.c",
         ])
+        .include("include")
+        .flag_if_supported("-g")
+        .flag_if_supported("-O3")
         .flag_if_supported("-pthread")
         .flag_if_supported("-std=c89")
-        .flag_if_supported("-O3")
         .flag_if_supported("-Wall")
-        .flag_if_supported("-g")
-        .include(base_dir.join("include/"))
-        .warnings(true)
         .compile("argon2");
 
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let out_dir = Path::new(&out_dir);
     let bindings = bindgen::Builder::default()
         .header("include/argon2.h")
-        .rustfmt_bindings(false)
-        .rust_target(bindgen::RustTarget::Stable_1_25)
         .layout_tests(true)
+        .rustfmt_bindings(false)
         .generate()
         .expect("failed to generate bindings");
     bindings
--- examples/bindgen.rs
+++ examples/bindgen.rs
@@ -4,17 +4,15 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
-    let out_dir = Path::new("/Users/bcmyers/dev/a2/data");
+    let out_dir = Path::new("./data");
     if out_dir.exists() {
         fs::remove_dir_all(out_dir).unwrap();
     }
     fs::create_dir_all(out_dir).unwrap();
-
     let bindings = bindgen::Builder::default()
         .header("include/argon2.h")
-        .rustfmt_bindings(false)
-        .rust_target(bindgen::RustTarget::Stable_1_25)
         .layout_tests(false)
+        .rustfmt_bindings(false)
         .generate()
         .expect("failed to generate bindings");
     bindings
